044e44840bf5b55a2d486f90e09a3c6942936682
shrikant215 <shrikantjk3@gmail.com>
2025-01-22 22:36:30 +0530

    Added client link

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(cors({ origin: 'https://podvibe-srjk-91bde6.netlify.app/', credentials: true }));
 app.use(express.json());
 app.use(
   session({
